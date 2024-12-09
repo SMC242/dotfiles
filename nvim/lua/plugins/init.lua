8e22fc501dc1d29d31d3c7fce981036c2ad0fc80
SMC242 <56912943+SMC242@users.noreply.github.com>
2024-12-09 02:15:33 +0000

    fix(nvim)!: lazy load Rust keybinds
    Only load Rust keybinds once Rustaceanvim loads. Previously, the
    keybinds were being used as a trigger for Rustaceanvim and were
    colliding with other languages' keybinds

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,8 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+Keymap = vim.keymap.set
+
 return {
   {
     "stevearc/conform.nvim",
@@ -82,42 +84,27 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5",
     lazy = false,
-    keys = {
-      {
-        "<leader>ca",
-        function()
-          vim.cmd.RustLsp "codeAction"
-        end,
+    config = function()
+      Keymap("n", "<leader>ca", function()
+        vim.cmd.RustLsp "codeAction"
+      end, { silent = true, desc = "LSP Rust code action" })
+
+      Keymap("n", "K", function()
+        vim.cmd.RustLsp { "hover", "actions" }
+      end, {
         silent = true,
         desc = "LSP Rust code action",
-      },
-      {
-        "K",
-        function()
-          vim.cmd.RustLsp { "hover", "actions" }
-        end,
-        silent = true,
-        desc = "LSP Rust hover",
-      },
-      {
-        "<leader>ex",
-        function()
-          vim.cmd.RustLsp { "explainError", "current" }
-        end,
-        silent = true,
-        mode = "n",
-        desc = "LSP Rust explain error",
-      },
-      {
-        "<leader>er",
-        function()
-          vim.cmd.RustLsp { "renderDiagnostic", "current" }
-        end,
-        silent = true,
-        mode = "n",
-        desc = "LSP Rust render error",
-      },
-    },
+      })
+
+      Keymap("n", "<leader>ex", function()
+        vim.cmd.RustLsp { "explainError", "current" }
+      end, { desc = "LSP Rust explain error" })
+
+      Keymap("n", "<leader>er", function()
+        vim.cmd.RustLsp { "renderDiagnostic", "current" }
+      end, { silent = true, desc = "LSP Rust render error" })
+    end,
+    ,
   },
   {
     "mrcjkb/haskell-tools.nvim",
