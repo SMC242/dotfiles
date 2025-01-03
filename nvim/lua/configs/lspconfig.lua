bb791a785042a0bc0f84519d6c6bb02059ac630f
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-01-03 21:00:54 +0000

    fix(nvim): set up emmet language server
    The language server wasn't running so Emmet wasn't working

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,19 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "bashls", "ts_ls", "eslint", "yamlls", "jsonls", "gopls", "clangd" }
+local servers = {
+  "html",
+  "cssls",
+  "pyright",
+  "bashls",
+  "ts_ls",
+  "eslint",
+  "yamlls",
+  "jsonls",
+  "gopls",
+  "clangd",
+  "emmet_language_server",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
