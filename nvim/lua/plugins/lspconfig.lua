6d92c02843b33e5cc973892e9564af17ad2a9460
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-02-12 19:22:58 +0000

    feat(nvim): add global go to references and implentations keys

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -9,6 +9,8 @@ return {
     -- Custom keybinds
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP Show help" })
     Keymap("n", "K", vim.lsp.buf.hover, { silent = true, noremap = true })
-    Keymap("n", "<leader>gd", vim.lsp.buf.definition, { silent = true, desc = "LSP Go to definition" })
+    Keymap("n", "<leader>gd", vim.lsp.buf.definition, { silent = true, noremap = true, desc = "LSP Go to definition" })
+    Keymap("n", "gr", vim.lsp.buf.references, { silent = true, noremap = true, desc = "LSP Go to references" })
+    Keymap("n", "gi", vim.lsp.buf.implementation, { silent = true, noremap = true, desc = "LSP Go to implementations" })
   end,
 }
