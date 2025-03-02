48c74f21914aedf356c7f0087881ae896916d62a
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-03-02 18:49:31 +0000

    feat(nvim)!: remove leader key from goto definition keybind

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@ return {
     -- Custom keybinds
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP Show help" })
     Keymap("n", "K", vim.lsp.buf.hover, { silent = true, noremap = true })
-    Keymap("n", "<leader>gd", vim.lsp.buf.definition, { silent = true, noremap = true, desc = "LSP Go to definition" })
+    Keymap("n", "gd", vim.lsp.buf.definition, { silent = true, noremap = true, desc = "LSP Go to definition" })
     Keymap("n", "gr", vim.lsp.buf.references, { silent = true, noremap = true, desc = "LSP Go to references" })
     Keymap("n", "gi", vim.lsp.buf.implementation, { silent = true, noremap = true, desc = "LSP Go to implementations" })
   end,
