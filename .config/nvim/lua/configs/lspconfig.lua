b27b64adb96b2af2d604723a9d74d21f741e48eb
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-10-09 18:41:39 +0100

    refactor(nvim): use native nvim lsp
    nvim-lspconfig got moved to the vim.lsp API

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -3,38 +3,43 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+require("nvchad.configs.lspconfig").defaults()
+
+local bicep_lsp_path = "/home/eilidhm/.local/share/nvim/mason/packages/bicep-lsp/bicep-lsp"
+
 local servers = {
-  "html",
-  "cssls",
-  "pyright",
-  "bashls",
-  "ts_ls",
-  "eslint",
-  "yamlls",
-  "jsonls",
-  "gopls",
-  "clangd",
-  "emmet_language_server",
-  "tailwindcss",
-  "astro",
-  "bicep",
+  html = {},
+  cssls = {},
+  pyright = {},
+  bashls = {},
+  ts_ls = {},
+  eslint = {},
+  yamlls = {},
+  jsonls = {},
+  gopls = {},
+  clangd = {},
+  emmet_language_server = {},
+  tailwindcss = {},
+  astro = {},
+  bicep = {
+    -- Mason installs a script that runs dotnet, so ignoring the lspconfig docs
+    -- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#bicep
+    cmd = { bicep_lsp_path },
+  },
 }
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  if lsp == "bicep" then
-    -- Mason installs a script that runs dotnet, so ignoring the lspconfig docs
-    -- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#bicep
-    local bicep_lsp_path = "/home/eilidhm/.local/share/nvim/mason/packages/bicep-lsp/bicep-lsp"
-    lspconfig["bicep"].setup {
-      cmd = { bicep_lsp_path },
-    }
-  else
-    lspconfig[lsp].setup {
-      on_attach = on_attach,
-      on_init = on_init,
-      capabilities = capabilities,
-    }
-  end
+for lsp, config in pairs(servers) do
+  config["on_attach"] = on_attach
+  config["on_init"] = on_init
+  config["capabilities"] = capabilities
+
+  vim.lsp.config[lsp] = config
 end
+
+local lsp_names = {}
+for lsp, _ in pairs(servers) do
+  table.insert(lsp_names, lsp)
+end
+
+vim.lsp.enable(lsp_names)
