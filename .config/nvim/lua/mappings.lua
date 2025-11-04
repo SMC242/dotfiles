beca1a9f2f515a5cf88373164c64cc1011895116
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-11-04 19:46:51 +0000

    Revert "fix(nvim): change break line to CTRL + SHIFT + J"
    This reverts commits 46bee403150ab33a5fc24fff81b96d9a61c8b877 and
    8f5446d83b8d3b74772cf3f5d5125c003457558e because the keybind was still
    conflicting with the window navigate down bind

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,7 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<C-J>", "i<CR><ESC>", { noremap = true })
 vim.api.nvim_create_user_command("BufOnly", "%bd|e#", { desc = "Buffer Delete all buffers except this one" })
 vim.api.nvim_create_user_command("BufNone", "%bd", { desc = "Buffer Delete all buffers" })
 
