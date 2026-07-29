1d6b2a7b300623768e4c862fa5c72caa4bceec71
SMC242 <56912943+SMC242@users.noreply.github.com>
2026-07-29 11:27:14 +0100

    feat(tmux): handle wikis in open-git-remote

--- tmux/open-git-remote.sh
+++ tmux/open-git-remote.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 cd "$1" || exit 1
-url=$(git remote get-url origin)
+url=$(git remote get-url origin | tr -d "\n")
+echo -n "Original URL: $url"
+if [[ "$url" =~ \.wiki(\.git)?$ ]]; then
+  echo "Matched"
+  url=$(echo -n "$url" | sed -r "s/\.wiki(\.git)?/\/wiki/")
+else
+  echo "Did not match"
+fi
 
+
+echo -n "$url"
 xdg-open "$url" || open "$url"
