4f514ca1d383404f17efe04bb0132b5b0f0b3d97
SMC242 <56912943+SMC242@users.noreply.github.com>
2026-07-29 11:32:19 +0100

    refactor(tmux): remove unncessary echos

--- tmux/open-git-remote.sh
+++ tmux/open-git-remote.sh
@@ -2,14 +2,9 @@
 
 cd "$1" || exit 1
 url=$(git remote get-url origin | tr -d "\n")
-echo -n "Original URL: $url"
 if [[ "$url" =~ \.wiki(\.git)?$ ]]; then
-  echo "Matched"
-  url=$(echo -n "$url" | sed -r "s/\.wiki(\.git)?/\/wiki/")
-else
-  echo "Did not match"
+  url=$(sed -r "s/\.wiki(\.git)?/\/wiki/" <<< "$url")
 fi
 
 
-echo -n "$url"
 xdg-open "$url" || open "$url"
