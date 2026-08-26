db198bbd2baa01afdc03c712a2089561c275d9bd
SMC242 <56912943+SMC242@users.noreply.github.com>
2026-08-26 23:07:30 +0100

    fix(nvim): re-add rust inline diagnostics
    They removed support and recommend using this plugin

---
+++ .config/nvim/lua/plugins/nvim-lsp-endhints.lua
@@ -0,0 +1,8 @@
+-- I added this because rustaceanvim removed inlay hint (inline diagnostics) support
+-- at the recommendation of its docs
+-- See https://github.com/mrcjkb/rustaceanvim#can-i-display-inlay-hints-to-the-end-of-the-line
+return {
+  "chrisgrieser/nvim-lsp-endhints",
+  event = "LspAttach",
+  opts = {}, -- required, even if empty
+}
